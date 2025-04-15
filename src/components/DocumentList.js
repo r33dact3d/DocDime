e7727fe8758ae72a3b67f120b8a6112ff34deaa9
r33dact3d <140332993+r33dact3d@users.noreply.github.com>
2025-04-15 13:23:19 -0500

    Update DocumentList.js

--- src/components/DocumentList.js
+++ src/components/DocumentList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-function DocumentList({ documents, handleBuy }) {
+function DocumentList({ documents, handleBuy, bsvPrice }) {
   return (
     <ul>
       {documents.map((doc, index) => (
         <li key={index}>
-          <span>{doc.name} - {doc.price} BSV</span>
-          <button onClick={() => handleBuy(doc)}>Buy</button>
+          <span>
+            {doc.name} - ${doc.price.toFixed(2)} USD{' '}
+            {bsvPrice ? `(${(doc.price / bsvPrice).toFixed(8)} BSV)` : '(Loading...)'}
+          </span>
+          <button onClick={() => handleBuy(doc)} disabled={!bsvPrice}>
+            Buy
+          </button>
         </li>
       ))}
     </ul>
