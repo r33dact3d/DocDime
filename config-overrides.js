f3cf0955cb95f8f3f6cc49a480af3ab2c30a3355
r33dact3d <140332993+r33dact3d@users.noreply.github.com>
2025-04-16 13:04:10 -0500

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,18 @@
+const webpack = require('webpack');
+
 module.exports = function override(config) {
   config.resolve.fallback = {
-    "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
-    "assert": require.resolve("assert"),
-    "buffer": require.resolve("buffer"),
-    "process": require.resolve("process")
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert'),
+    buffer: require.resolve('buffer'),
+    process: require.resolve('process/browser'),
   };
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    })
+  );
   return config;
 };
