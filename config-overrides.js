2ad2ae8ebbae95690753fd0591096c97228a27d6
r33dact3d <140332993+r33dact3d@users.noreply.github.com>
2025-04-16 13:50:16 -0500

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,13 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
-  config.resolve.fallback = {
-    crypto: require.resolve('crypto-browserify'),
-    stream: require.resolve('stream-browserify'),
-    assert: require.resolve('assert'),
-    buffer: require.resolve('buffer'),
-    process: require.resolve('process/browser.js'), // Explicit extension for ESM
-  };
   config.plugins.push(
     new webpack.ProvidePlugin({
-      process: 'process/browser.js',
+      process: 'process',
       Buffer: ['buffer', 'Buffer'],
     }),
     new webpack.DefinePlugin({
-      'process.env': JSON.stringify({}), // Minimal env polyfill
+      'process.env': JSON.stringify({}),
     })
   );
   return config;
